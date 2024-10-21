3af519613012008c5b0d61fe7ddf052979836489
geraldine-l <geraldine.lonchay@gmail.com>
2024-10-21 03:51:16 +0200

    add state1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import PokemonCard from "./components/PokemonCard";
 import "./App.css";
+import { useState } from "react";
 
 const pokemonList = [
 	{
@@ -8,15 +9,41 @@ const pokemonList = [
 			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
 	},
 	{
+		name: "charmander",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+	},
+	{
+		name: "squirtle",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+	},
+	{
+		name: "pikachu",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+	},
+	{
 		name: "mew",
 	},
 ];
 
 function App() {
+	const [pokemonIndex, setPokemonIndex] = useState(0);
+
 	return (
 		<>
-			<div>
-				<PokemonCard pokemon={pokemonList[1]} />
+			<div className="App">
+				<PokemonCard pokemon={pokemonList[pokemonIndex]} />
+				<p>Using states</p>
+				<div>
+					<button onClick={() => setPokemonIndex(pokemonIndex - 1)}>
+						Précédent
+					</button>
+					<button onClick={() => setPokemonIndex(pokemonIndex + 1)}>
+						Suivant
+					</button>
+				</div>
 			</div>
 		</>
 	);
